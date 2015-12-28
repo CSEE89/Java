033c084d07e6f797955b3b3c0e6394de57c7ce82
cs <flashcsabi@gmail.com>
2015-12-29 00:13:04 +0100

    eventhandler

--- FirstJavaFX/src/pak/Main.java
+++ FirstJavaFX/src/pak/Main.java
@@ -1,11 +1,38 @@
 package pak;
 
 import javafx.application.*;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 
-public class Main {
+public class Main extends Application implements EventHandler{
 
+	Button button;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.setTitle("Title of the wi");
+		button = new Button();
+		button.setText("button");
+		button.setOnAction(this);
+		StackPane layout = new StackPane();
+		layout.getChildren().add(button);
+		
+		Scene scene = new Scene(layout,200,300);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+
+	@Override
+	public void handle(Event arg0) {
+		button.setText("Clicled");
 		
 	}
 
